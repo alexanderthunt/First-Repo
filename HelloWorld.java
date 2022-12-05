c8da7f899410c2292bf52aa8cc834d7d687f68da
alexanderthunt <alexander056@revature.net>
2022-12-05 16:29:51 -0500

    third commit with java file

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        boolean bool = true;
+        while(bool) {
+            System.out.println("Hello World!");
+            bool = false;
+        }
+    }
+}
